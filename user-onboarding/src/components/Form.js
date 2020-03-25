69d9b63674bd6f738464e04e36769bec0cf73626
gabeaster <gab.easter@live.com>
2020-03-25 16:22:19 -0400

    state for errors and post request setup, input change setup, post request done; next: setting up validation

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -2,19 +2,92 @@ import React from "react";
 import axios from "axios";
 import * as yup from "yup";
 
+const formSchema = yup.object().shape({
+    name: yup
+        .string()
+        .required("Everyone has a name, my friend"),
+    email: yup
+        .string()
+        .email("Something's not right, my friend.")
+        .required("Gotta have your email, my friend."),
+    password: yup
+        .string()
+        .required("Any password will do, my friend."),
+    role: yup
+        .string(),
+    terms: yup  
+        .boolean().oneOf([true], "Agree to the Terms or Turn Away Now")
 
+});
 
 export default function Form() {
 
-    // //managing state for our form inputs
-    // const [formState, setFormState] = useState({
-    //     name: '',
-    //     email: '',
-    //     password: '',
-    //     role: '',
-    //     terms: ''
-    // });
+    //managing state for our form inputs
+    const [formState, setFormState] = useState({
+        name: '',
+        email: '',
+        password: '',
+        role: '',
+        terms: ''
+    });
+    //state for Errors
+    const [errors, setErrors] = useState({
+        name: '',
+        email: '',
+        password: '',
+        role: '',
+        terms: ''
+    });
+    //state for Post request
+    const [post, setPost] = useState([]);
 
+
+    //input Change
+    const inputChange = e => {
+        e.persist();
+        const newFormData = {
+            ...formState,
+            [e.target.name] : e.target.type === "checkbox" ? e.target.checked : e.target.value
+        };
+
+        validateChange(e);
+        setFormState(newFormData);
+    }
+    //validation
+    const validateChange = e => {
+        yup
+            .reach(formSchema, e.target.name)
+            .validate(e.target.value)
+            .then(valid => {
+                setErrors({
+                    ...errors, [e.target.name] : ''
+                });
+            })
+            .catch(err => {
+                setErrors({
+                    ...errors, [e.target.name]: err.errors[0]
+                });
+            });
+    };
+    //form submit with post request
+    const formSubmit = e => {
+        e.preventDefault();
+        axios 
+            .post('https://reqres.in/api/users', formState)
+            .then( res => {
+                setPost(res.data);
+                console.log("success", post);
+                setFormState({
+                    name: '',
+                    email: '',
+                    password: '',
+                    role: '',
+                    terms: ''
+                });
+            })
+            .catch(err => console.log(err.response))
+
+    }
 
 
 
@@ -25,27 +98,36 @@ export default function Form() {
                 <input 
                     type='text'
                     name='name'
-                    value=''
-                    onChange=''
+                    value={formState.name}
+                    onChange={inputChange} 
                 />
             </label>
-            <label htmlFor="">
-
-                <input 
-
+            <label htmlFor="email">
+                Email
+                <input
+                    type='text'
+                    name='email'
+                    value={formState.email}
+                    onChange={inputChange} 
                 />
             </label>
-            <label htmlFor="">
-
+            <label htmlFor="password">
+                Password
                 <input 
-
+                    type='text'
+                    name='name'
+                    value={formState.password}
+                    onChange={inputChange} 
                 />
             </label>
-            <label htmlFor="">
-
-                <input 
-
-                />
+            <label htmlFor="role">
+                Role
+                <select id="roles" name="roles" onChange={inputChange}>
+                    <option value="Front-End"> Front-End</option>
+                    <option value="Back-End">Back-End</option>
+                    <option value="UI/UX Designer">UI/UX Designer</option>
+                    <option value="Project Manager">Project Manager</option>
+                </select>
             </label>
             <label htmlFor="">
 

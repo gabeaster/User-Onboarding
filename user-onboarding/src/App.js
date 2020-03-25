d53fab3bf4e9fb41527ad254c502e40043e1c8f8
gabeaster <gab.easter@live.com>
2020-03-25 15:49:22 -0400

    user-onboarding file structure setup, component called Form.js imported into App.js. Moving on to step 1- creating advanced form

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Sign Up!</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ user-onboarding/src/components/Form.js
@@ -0,0 +1,11 @@
+import React from "react";
+import axios from "axios";
+import * as yup from "yup";
+
+export default function Form() {
+    return (
+        <form>
+            
+        </form>
+    );
+}

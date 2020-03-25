20b7218bddbcb9058bad2023ae35fbb160b6743f
gabeaster <gab.easter@live.com>
2020-03-25 16:07:03 -0400

    formSchema setup, basic form outline in return statement complete

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -2,10 +2,59 @@ import React from "react";
 import axios from "axios";
 import * as yup from "yup";
 
+
+
 export default function Form() {
+
+    // //managing state for our form inputs
+    // const [formState, setFormState] = useState({
+    //     name: '',
+    //     email: '',
+    //     password: '',
+    //     role: '',
+    //     terms: ''
+    // });
+
+
+
+
     return (
         <form>
-            
+            <label htmlFor="name">
+                Name
+                <input 
+                    type='text'
+                    name='name'
+                    value=''
+                    onChange=''
+                />
+            </label>
+            <label htmlFor="">
+
+                <input 
+
+                />
+            </label>
+            <label htmlFor="">
+
+                <input 
+
+                />
+            </label>
+            <label htmlFor="">
+
+                <input 
+
+                />
+            </label>
+            <label htmlFor="">
+
+                <input 
+
+                />
+            </label>
+            {/* <pre>{JSON.stringify(post, null, 3)}</pre> */}
+            <button>Submits</button>
         </form>
     );
 }

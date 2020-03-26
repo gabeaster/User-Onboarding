409379304164eb6d60c422962ca0414776e76694
gabeaster <gab.easter@live.com>
2020-03-26 16:01:48 -0400

    name test passed and asserted; email test failed at get. next: getting email test to pass and working on password test.

--- user-onboarding/cypress/integration/form_test.js
+++ user-onboarding/cypress/integration/form_test.js
@@ -1,5 +1,15 @@
 describe("Testing inputs and submit for form", function() {
     beforeEach(function() {
-        crypto.visit("http://localhost:3000/");
+        cy.visit("http://localhost:3000/");
+    });
+    it("Completes form and submits", function() {
+        cy
+            .get('input[name="name"]')
+            .type("name")
+            .should("have.value", "name");
+        cy
+            .get('input[name="email]')
+            .type("email@email.com")
+            .should("have.value", "email@email.com");
     });
 });
